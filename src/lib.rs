8b42b3d6cfc2daebd9e2d6544fc779b75d25fbb1
Yummiii <Leeo05050@protonmail.com>
2024-04-06 10:31:29 -0300

    isso vai fazer sentido alguma hr

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Error;
 use aoba::Aoba;
 use arkalis::Arkalis;
 
@@ -5,13 +6,20 @@ pub mod aoba;
 pub mod arkalis;
 pub mod repos;
 
-pub async fn get_clients(
-    arkalis_url: &str,
-    aoba_url: &String,
-    token: &Option<String>,
-) -> anyhow::Result<(Arkalis, Aoba)> {
-    let arkalis = Arkalis::new(arkalis_url, token).await?;
-    let aoba = Aoba::start(aoba_url, token)?;
+pub struct Clients {
+    pub aoba: Result<Aoba, Error>,
+    pub arkalis: Result<Arkalis, Error>,
+}
 
-    Ok((arkalis, aoba))
+impl Clients {
+    pub async fn new_clients(
+        arkalis_url: &str,
+        aoba_url: &String,
+        token: &Option<String>,
+    ) -> Clients {
+        let arkalis = Arkalis::new(arkalis_url, token).await;
+        let aoba = Aoba::start(aoba_url, token);
+
+        Clients { arkalis, aoba }
+    }
 }
